2ca4256100535d1ad2cbe555568cc257622b45a2
Athul030 <techmobo2@gmail.com>
2024-03-25 14:37:35 +0530

    mongodb changed to clusters v2

--- src/main/java/com/athul/memegramspring/MemegramSpringApplication.java
+++ src/main/java/com/athul/memegramspring/MemegramSpringApplication.java
@@ -5,6 +5,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
